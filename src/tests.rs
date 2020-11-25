8198b3341a836b9a6eebfcdd7356c649cdf44ec5
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2020-11-24 17:00:44 -0800

    feat: Add rank template

--- src/tests.rs
+++ src/tests.rs
@@ -17,7 +17,29 @@ async fn template_responds_with_image() {
         .header(ContentType::JSON)
         .remote("127.0.0.1:8000".parse().unwrap())
         .body(
-            r#"{"name": "test", "width": 1280, "height": 720, "ctx": {"name": "Bob", "age": 30 }}"#,
+            r#"{
+                "name": "rank",
+                "width": 500,
+                "height": 400,
+                "ctx": {
+                    "CONTENT_COLOR": "0, 184, 148",
+                    "CONTENT_OPACITY": "1",
+                    "AVATAR_URL": "https://cdn.discordapp.com/avatars/150443906511667200/afaddec4029eafd36e30fb62efe7bfad.png",
+                    "REP": "123",
+                    "FISHIES": "456",
+                    "USERNAME": "test#1234",
+                    "PATRON_EMOJI": "",
+                    "XP_PROGRESS": "75",
+                    "LEVEL": "10",
+                    "GLOBAL_LEVEL": "15",
+                    "CURR_LEVEL_XP": "125",
+                    "LEVEL_XP_REQ": "1200",
+                    "DAILY": "60 / 67",
+                    "WEEKLY": "87 / 892",
+                    "MONTHLY": "113 / 5139",
+                    "ALL": "489 / 33067"
+                }
+            }"#
         )
         .dispatch()
         .await;
